9a586c5d3fd602452714d77d60ca8d92b88121cb
Nukepayload2 <1939357182@qq.com>
2019-10-07 19:48:15 +0800

    七老爷御魂整理方案作为工具发布

--- 测试程序/Program.vb
+++ 测试程序/Program.vb
@@ -1,25 +1,30 @@
-﻿Imports Nukepayload2.Linq.Onmyoji
+﻿Imports System.IO
+Imports Nukepayload2.Linq.Onmyoji
+Imports Nukepayload2.Linq.Onmyoji.Utilities
 
 Module Program
-    Sub Main()
-        Dim 快照 = 痒痒熊快照.加载Json文件("测试数据.json")
-        Dim 五星御魂 =
-            From s In 快照.数据.御魂 Where s.星级 = 5 AndAlso s.已弃置 = False
-        Console.WriteLine($"整理前五星御魂数量: {五星御魂.Count}")
-        清理五星御魂(快照)
-        Console.WriteLine($"整理后五星御魂数量: {五星御魂.Count}")
+    Sub Main(args As String())
+        Dim 输入文件 = args.FirstOrDefault
+        Try
+            If 输入文件 = Nothing OrElse Not File.Exists(输入文件) Then
+                Console.WriteLine("需要拖放痒痒熊快照 Json 文件")
+                Return
+            End If
+
+            Console.WriteLine("载入中...")
+            Dim 快照 = 痒痒熊快照.加载Json文件(输入文件)
+
+            Dim 六星御魂 = From s In 快照.数据.御魂 Where s.星级 = 6 AndAlso s.已弃置 = False
+
+            Console.WriteLine($"整理前六星御魂数量: {六星御魂.Count}")
+            御魂整理方案.七老爷三周年庆御魂整理方案(快照)
+            Console.WriteLine($"整理后六星御魂数量: {六星御魂.Count}")
+        Catch ex As Exception
+            Console.WriteLine($"出现错误: {ex.Message}")
+        Finally
+            Console.WriteLine("按任意键退出...")
+            Console.ReadKey()
+        End Try
     End Sub
 
-    Private Sub 清理五星御魂(快照 As 痒痒熊快照)
-        With 快照.数据.御魂.创建御魂整理
-            .星级.选择(5)
-            .副属性条数 = 副属性条数.两条
-            .全选.弃置
-            .副属性条数 = 副属性条数.不限
-            .副属性没有.选择(御魂属性类型.暴击伤害)
-            .全选.弃置
-            .副属性没有.改为(御魂属性类型.暴击)
-            .全选.弃置
-        End With
-    End Sub
 End Module
