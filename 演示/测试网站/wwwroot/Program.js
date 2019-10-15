8368a346af987151db968981259614c83a1d404f
Nukepayload2 <1939357182@qq.com>
2019-10-15 22:57:22 +0800

    提升版本到 1.0 pre 4

---
+++ 演示/测试网站/wwwroot/Program.js
@@ -0,0 +1,10 @@
+﻿window.setProperty = function (element, property, value) {
+    element[property] = value
+}
+window.clickElement = function (element) {
+    element.click()
+}
+window.saveAsBlob = function (blobBody, fileName) {
+    var blob = new Blob([String.fromCharCode(0xFEFF), blobBody], { type: "text/csv;charset=utf-8" })
+    window.saveAs(blob, fileName)
+}
--- 演示/演示WPF程序/Views/MainWindow.TitleBar.vb
+++ 演示/演示WPF程序/Views/MainWindow.TitleBar.vb
@@ -1,7 +1,7 @@
 ﻿Partial Class MainWindow
 
     Private Sub BtnAbout_Click(sender As Object, e As RoutedEventArgs) Handles BtnAbout.Click
-        MsgBox("版本 1.0 preview 3, 适配痒痒熊导出器 0.99.1。
+        MsgBox("版本 1.0 preview 4, 适配痒痒熊导出器 0.99.1。
 作者: 
 B站、GitHub、百度贴吧、微博：Nukepayload2。
 阴阳师：依偎相守#2723416
