6b2d8f0ba6d51bfef48cb32baca308a561e14c86
Nukepayload2 <1939357182@qq.com>
2020-04-04 15:32:49 +0800

    增加御魂图标

--- 演示/御魂方案录制宏/御魂方案录制宏桌面版/Controls/EquipmentPresenter.xaml.vb
+++ 演示/御魂方案录制宏/御魂方案录制宏桌面版/Controls/EquipmentPresenter.xaml.vb
@@ -1,3 +1,18 @@
-﻿Public Class EquipmentPresenter
+﻿Imports Nukepayload2.Linq.Onmyoji
+
+Public Class EquipmentPresenter
+
+    Public Property EquipmentType As 御魂种类
+        Get
+            Return GetValue(EquipmentTypeProperty)
+        End Get
+        Set
+            SetValue(EquipmentTypeProperty, Value)
+        End Set
+    End Property
+    Public Shared ReadOnly EquipmentTypeProperty As DependencyProperty =
+                           DependencyProperty.Register(NameOf(EquipmentType),
+                           GetType(御魂种类), GetType(EquipmentPresenter),
+                           New PropertyMetadata(御魂种类.破势))
 
 End Class
