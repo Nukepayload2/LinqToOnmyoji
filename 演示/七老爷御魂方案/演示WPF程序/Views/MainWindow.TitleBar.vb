ea62815ae2a5206dd773765029dbbbff8507c4e0
Nukepayload2 <1939357182@qq.com>
2020-04-12 19:31:44 +0800

    更新 DPI 设置

--- 演示/七老爷御魂方案/演示WPF程序/Views/MainWindow.TitleBar.vb
+++ 演示/七老爷御魂方案/演示WPF程序/Views/MainWindow.TitleBar.vb
@@ -3,7 +3,7 @@
 Partial Class MainWindow
 
     Private Sub BtnAbout_Click(sender As Object, e As RoutedEventArgs) Handles BtnAbout.Click
-        MsgBox($"版本 1.0 preview 8, 适配{痒痒熊快照.已适配的产品和版本}。
+        MsgBox($"版本 1.0, 适配{痒痒熊快照.已适配的产品和版本}。
 作者: 
 B站、GitHub、百度贴吧、微博：Nukepayload2。
 阴阳师：依偎相守#2723416
@@ -11,7 +11,7 @@ B站、GitHub、百度贴吧、微博：Nukepayload2。
 B站 解说七老爷,这里阿毛酱", vbInformation, "关于")
     End Sub
 
-    Private Sub BtnClose_Click(sender As Object, e As RoutedEventArgs)
+    Private Sub BtnClose_Click(sender As Object, e As RoutedEventArgs) Handles BtnClose.Click
         Close()
     End Sub
 
@@ -19,4 +19,7 @@ B站 解说七老爷,这里阿毛酱", vbInformation, "关于")
         Process.Start("explorer.exe", "https://github.com/Nukepayload2/LinqToOnmyoji")
     End Sub
 
+    Private Sub BtnMin_Click(sender As Object, e As RoutedEventArgs) Handles BtnMin.Click
+        WindowState = WindowState.Minimized
+    End Sub
 End Class
